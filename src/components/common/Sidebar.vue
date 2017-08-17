5c57d6c676d1a1b169623e15bd892fb366511d28
guohao <13524286564@163.com>
2017-08-17 21:47:36 +0800

    整理菜单

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -26,11 +26,6 @@
       return {
         items: [
           {
-            icon: 'el-icon-setting',
-            index: '/readme',
-            title: '自述'
-          },
-          {
             icon: 'el-icon-menu',
             index: '4',
             title: '财务管理',
@@ -77,57 +72,10 @@
             index: '/account',
             title: '账号管理',
           },
-          {
-            icon: 'el-icon-menu',
-            index: '2',
-            title: '表格',
-            subs: [
-              {
-                index: '/basetable',
-                title: '基础表格'
-              },
-              {
-                index: '/vuetable',
-                title: 'Vue表格组件'
-              }
-            ]
-          },
-          {
-            icon: 'el-icon-date',
-            index: '3',
-            title: '表单',
-            subs: [
-              {
-                index: '/baseform',
-                title: '基本表单'
-              },
-              {
-                index: '/vueeditor',
-                title: '编辑器'
-              },
-              {
-                index: '/markdown',
-                title: 'markdown'
-              },
-              {
-                index: '/upload',
-                title: '文件上传'
-              }
-            ]
-          },
-          {
-            icon: 'el-icon-star-on',
-            index: '/basecharts',
-            title: '图表'
-          },
+
           {
             icon: 'el-icon-upload2',
-            index: '/drag',
-            title: '拖拽'
-          },
-          {
-            icon: 'el-icon-upload2',
-            index: '5',
+            index: '6',
             title: '客户管理',
             subs: [
               {
@@ -144,7 +92,51 @@
             icon: 'el-icon-setting',
             index: '/preferences',
             title: '参数设置'
-          }
+          },
+          {
+            icon: 'el-icon-menu',
+            index: '2',
+            title: '例子',
+            subs: [
+              {
+                index: '/basetable',
+                title: '基础表格'
+              },
+              {
+                index: '/vuetable',
+                title: 'Vue表格组件'
+              },
+              {
+                icon: 'el-icon-setting',
+                index: '/readme',
+                title: '自述'
+              },
+              {
+                index: '/baseform',
+                title: '基本表单'
+              },
+              {
+                index: '/vueeditor',
+                title: '编辑器'
+              },
+              {
+                index: '/markdown',
+                title: 'markdown'
+              },
+              {
+                index: '/upload',
+                title: '文件上传'
+              },
+              {
+                index: '/basecharts',
+                title: '图表'
+              },
+              {
+                index: '/drag',
+                title: '拖拽'
+              }
+            ]
+          },
         ]
       }
     },
