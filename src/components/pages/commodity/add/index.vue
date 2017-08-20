b0a58d501570c2af519bfe7396133094ca9c0c0a
castawayy <1455322152@qq.com>
2017-08-20 23:20:11 +0800

    商品管理-新增

--- src/components/pages/commodity/add/index.vue
+++ src/components/pages/commodity/add/index.vue
@@ -1,93 +1,193 @@
-
 <template>
-  <div class="table">
-    <div class="crumbs">
-      <el-breadcrumb separator="/">
-        <el-breadcrumb-item>商品管理</el-breadcrumb-item>
-        <el-breadcrumb-item>列表</el-breadcrumb-item>
-      </el-breadcrumb>
-    </div>
-    <div class="handle-box">
-      <el-select v-model="value" placeholder="选择分类">
-        <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-      <el-select v-model="value" placeholder="选择状态">
-        <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-      <el-input v-model="select_word" placeholder="输入商品名称/编码" class="handle-input mr10"></el-input>
-      <el-button type="primary" icon="search" @click="search">搜索</el-button>
-      <div class="fl-r">
-        <el-button type="primary" @click="addClientUrl">添加</el-button>
+  <div id="commodityAdd">
+    <el-form ref="form" :model="form" label-width="80px">
+      <div class="clearFix" style="width: 90%;margin: 0 auto;">
+        <div class="baseInfoRule">
+          <span class="title">基本信息</span>
+          <template>
+            <el-radio class="form.radio" v-model="radio" label="1">备选项</el-radio>
+          </template>
+        </div>
+        <el-form-item label="商品名称" required class="width50 fl-l">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="排序值" class="width50 fl-l">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="商品分类" required class="width50 fl-l">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="商品重量" class="width50 fl-l">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位" required class="width50 fl-l">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="商品体积" class="width50 fl-l">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="搜索关键字" class="width50 fl-l">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="起订量" class="width50 fl-l">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
       </div>
-    </div>
-    <el-table :data="tableData" border style="width: 100%"
-              ref="multipleTable"
-              @selection-change="handleSelectionChange"
-              v-loading="loading">
-      <!--<el-table-column type="selection" width="55"></el-table-column>-->
-      <el-table-column prop="name" label="商品图片" width="150">
-        <template scope="scope">
-          <img v-bind:src="name" alt="" width="60" height="60">
-        </template>
-      </el-table-column>
-      <el-table-column prop="name" label="商品编码" sortable width="120">
-      </el-table-column>
-      <el-table-column prop="name" label="商品名称" sortable>
-      </el-table-column>
-      <el-table-column prop="name" label="商品规格">
-      </el-table-column>
-      <el-table-column prop="name" label="单位">
-      </el-table-column>
-      <el-table-column prop="name" label="市场价" sortable>
-      </el-table-column>
-      <el-table-column prop="name" label="成本参考价">
-      </el-table-column>
-      <el-table-column prop="name" label="库存数量">
-      </el-table-column>
-      <el-table-column prop="name" label="预购">
-      </el-table-column>
-      <el-table-column prop="name" label="库存成本">
-      </el-table-column>
-      <el-table-column prop="name" label="状态">
-      </el-table-column>
-      <el-table-column prop="name" label="新增时间">
-      </el-table-column>
-      <el-table-column prop="name" label="修改时间">
-      </el-table-column>
-      <el-table-column
-        fixed="right"
-        label="操作"
-        width="100">
-        <template scope="scope">
-          <el-button type="text" size="small">修改</el-button>
-          <el-button type="text" size="small">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <div class="pagination">
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="1"
-        :page-sizes="[30, 60, 120, 240]"
-        :page-size="10"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="240">
-      </el-pagination>
-    </div>
+      <div class="clearFix" style="width: 90%;margin: 0 auto;">
+        <div class="baseInfoRule">
+          <span class="title">商品规格</span>
+          <template>
+            <el-radio class="form.radio" v-model="radio" label="1">设置商品多规格</el-radio>
+          </template>
+        </div>
+        <el-table
+          :data="tableData"
+          style="width: 100%">
+          <el-table-column
+            label="备注"
+            width="100"
+            algin="center"
+            show-overflow-tooltip
+            align="center"
+          >
+            <template scope="scope">
+              <el-button style="text-align: center;font-size: 20px;color: #000"
+                         type="text" @click="dialogVisible = true" size="small">+</el-button>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="规格"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="商品编码">
+            <template scope="scope">
+              <el-input v-model="form.name"></el-input>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div class="clearFix" style="width: 90%;margin: 0 auto;">
+        <div class="baseInfoRule">
+          <span class="title">商品图片</span>
+        </div>
+          <el-upload
+            action="https://jsonplaceholder.typicode.com/posts/"
+            list-type="picture-card"
+            :on-preview="handlePictureCardPreview"
+            :on-remove="handleRemove">
+            <i class="el-icon-plus"></i>
+          </el-upload>
+      </div>
+      <div class="clearFix" style="width: 90%;margin: 0 auto;">
+        <div class="baseInfoRule">
+          <span class="title">出库设置</span>
+        </div>
+        <div>
+          <div style="width: 100%;margin-bottom: 15px;">
+            <template>
+                <el-checkbox v-model="form.checked"></el-checkbox>
+            </template>
+            <span style="padding-left: 35px;">
+              安徽省
+            </span>
+            <el-select v-model="value" placeholder="请选择" style="width: 200px;padding-left: 50px;">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+          <div style="width: 100%;margin-bottom: 15px;">
+            <template>
+              <el-checkbox v-model="form.checked"></el-checkbox>
+            </template>
+            <span style="padding-left: 35px;">
+              安徽省
+            </span>
+            <el-select v-model="value" placeholder="请选择" style="width: 200px;padding-left: 50px;">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+          <div style="width: 100%;margin-bottom: 15px;">
+            <template>
+              <el-checkbox v-model="form.checked"></el-checkbox>
+            </template>
+            <span style="padding-left: 35px;">
+              安徽省
+            </span>
+            <el-select v-model="value" placeholder="请选择" style="width: 200px;padding-left: 50px;">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+        </div>
+      </div>
+      <div class="clearFix" style="width: 90%;margin: 0 auto;">
+        <div class="baseInfoRule">
+          <span class="title">商品描述</span>
+        </div>
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 2, maxRows: 4}"
+          placeholder="请输入内容"
+          v-model="textarea">
+        </el-input>
+      </div>
+      <div class="clearFix" style="width: 90%;margin: 0 auto;">
+        <div class="baseInfoRule">
+          <span class="title">单价设置</span>
+        </div>
+        <div>
+          <div>
+            <el-form-item label="市场价" class="width50 fl-l">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+          </div>
+          <div>
+            <el-form-item label="参考成本价" class="width50 fl-l">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+          </div>
+        </div>
+      </div>
+      <el-dialog v-model="dialogVisible" size="tiny">
+        <img width="100%" :src="dialogImageUrl" alt="">
+      </el-dialog>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
-
 <script>
   export default {
     data() {
@@ -109,114 +209,104 @@
           label: '北京烤鸭'
         }],
         value: '',
-
-        loading: false,
-        url: './static/vuetable.json',
-        tableData: [],
-        cur_page: 1,
-        multipleSelection: [],
-        //select_cate: '',
-        select_word: '',
-        del_list: [],
-        is_search: false,
-      }
-    },
-    created(){
-      this.getData();
-    },
-    computed: {
-      data(){
-        const self = this;
-        return self.tableData.filter(function (d) {
-          let is_del = false;
-          for (let i = 0; i < self.del_list.length; i++) {
-            if (d.name === self.del_list[i].name) {
-              is_del = true;
-              break;
-            }
-          }
-          if (!is_del) {
-            if ((d.name.indexOf(self.select_word) > -1)) {
-              return d;
-            }
-          }
-        })
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }],
+        form: {
+          name: '',
+          radio:'1',
+          checked: true
+        },
+        dialogImageUrl: '',
+        dialogVisible: false,
+        textarea:''
       }
     },
     methods: {
-      handleCurrentChange(val){
-        this.cur_page = val;
-        this.getData();
+      onSubmit() {
+        console.log('submit!');
       },
-      handleSizeChange(val) {
-        console.log(`每页 ${val} 条`);
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
       },
-      getData(){
-        this.loading = true;
-        let self = this;
-        if (process.env.NODE_ENV === 'development') {
-          self.url = '/ms/table/list';
-        }
-        ;
-        self.$request.post(self.url, {page: self.cur_page}).then((res) => {
-          self.tableData = res.list;
-          self.loading = false;
-        }).catch(function (err) {
-          self.loading = false;
-          console.log(err)
-        })
-      },
-      search(){
-        this.is_search = true;
-      },
-      formatter(row, column) {
-        return row.address;
-      },
-      filterTag(value, row) {
-        return row.tag === value;
-      },
-      ddd(){
-        const self = this,
-          length = self.multipleSelection.length;
-        let str = '';
-        console.log(JSON.stringify(self.multipleSelection))
-        //self.del_list = self.del_list.concat(self.multipleSelection);
-//        for (let i = 0; i < length; i++) {
-//          str += self.multipleSelection[i].name + ' ';
-//        }
-//        self.$message.error('删除了' + str);
-//        self.multipleSelection = [];
-        console.log(self.multipleSelection)
-      },
-      handleSelectionChange(val) {
-        this.multipleSelection = val;
-      },
-      addClientUrl () {
-        this.$router.push('/commodity/add')
-      },
-      handleCommand(command) {
-          this.handleCurrentChange()
-        this.$message('click on item ' + command);
+      handlePictureCardPreview(file) {
+        this.dialogImageUrl = file.url;
+        this.dialogVisible = true;
       }
     }
   }
 </script>
+<style>
+  .clearFix:after{
+    clear:both;
+    display:block;
+    visibility:hidden;
+    height:0;
+    line-height:0;
+    content:'';
 
-<style scoped>
-  .handle-box {
-    margin-bottom: 20px;
   }
-
-  .handle-select {
-    width: 120px;
+  .clearFix{
+    zoom:1;
+    clear: both;
+  } /*解决ie6/7兼容问题*/
+#commodityAdd .width50{
+  width: 50%;
+}
+  #commodityAdd .fl-l{
+    float: left;
   }
-
-  .handle-input {
-    width: 300px;
-    display: inline-block;
+  #commodityAdd .baseInfoRule{
+    width: 100%;
+    float: left;
+    margin: 30px 0 35px 0;
   }
-  .fl-r{
-    float: right;
+  #commodityAdd .baseInfoRule span.title{
+    font-size: 18px;
+    color: #000000;
+    padding-right: 50px;
+  }
+  #commodityAdd .el-form-item__label{
+    width: 16% !important;
+  }
+  #commodityAdd .el-select{
+    width: 100%;
+  }
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #20a0ff;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
   }
 </style>
-
